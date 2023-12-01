0cd0edda10c5a6c38ec104c77aeb2dec38bb36ce
Amogh-2404 <ramogh2404@gmail.com>
2023-12-01 19:14:46 +0530

    inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '81e705d8-e3c4-4356-bdb8-dfaadfedc6b3';
+export const MODAL_CLOSE_SEC = 2.5;
